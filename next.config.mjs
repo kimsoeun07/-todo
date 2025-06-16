6d43d543e0b9e7be3ca6d458dee411b87ff2791c
kimsoeun07 <110821356+kimsoeun07@users.noreply.github.com>
2025-06-16 17:42:49 +0900

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    eslint:{
+        ignoreDuringBuilds : true,
+        // 스타일 검사 안할거임 ~ 무시할거야 라는 뜻!
+    }
+};
 
 export default nextConfig;
